fb69676e61d149684ca257166548f0c65e1f37a7
Gabriel Hidasy <gabrielhidasy@gmail.com>
2013-06-30 16:47:12 -0300

    some errors corrected

--- lab6.c
+++ lab6.c
@@ -5,13 +5,13 @@
 
 int main (int argc, char *argv[]) {
 
-  ext2_filsys * gagatinho = NULL;
+  ext2_filsys gagatinho;
   int error, bit1, bit2, n_seg, tam;
-  ext2fs_block_bitmap* map;
+  ext2fs_block_bitmap map;
   blk_t block_start, block_end;
   io_manager unix_io;
  
-  error = ext2fs_open(argv[1], EXT2_FLAG_RW, 0, 4096, unix_io_manager, gagatinho); //EXT2_FLAG_RW flag que determina apenas leitura e escrita | superbloco 0 determina que o superbloco primário 1024 será usado
+  error = ext2fs_open(argv[1], EXT2_FLAG_FORCE, 0, 0, unix_io_manager, &gagatinho); //EXT2_FLAG_RW flag que determina apenas leitura e escrita | superbloco 0 determina que o superbloco primário 1024 será usado
   if(error != 0){
     printf("Erro na abertura do sistema de arquivos.\n");
     return 0;
@@ -19,18 +19,18 @@ int main (int argc, char *argv[]) {
 
   if(argc == 2){
    
-    error = ext2fs_allocate_block_bitmap (*gagatinho, BLOCK_FLAG_DATA_ONLY, map);
+    error = ext2fs_allocate_block_bitmap (gagatinho, "in-use block map", &map);
     if(error != 0){
       printf("Erro na alocação do bitmap.\n");
       return 0;
     }
    
-    block_start = ext2fs_get_block_bitmap_start (*map);
-    block_end = ext2fs_get_block_bitmap_end (*map);
+    block_start = ext2fs_get_block_bitmap_start (map);
+    block_end = ext2fs_get_block_bitmap_end (map);
    
     for(bit2=0, n_seg=0; block_start <= block_end;  block_start++){
       bit1=bit2;
-      bit2 = ext2fs_test_block_bitmap (*map, block_start);
+      bit2 = ext2fs_test_block_bitmap (map, block_start);
       if(bit1==0&&bit2==0){
 	n_seg++;
       } else {
@@ -51,7 +51,7 @@ int main (int argc, char *argv[]) {
   } else {
     printf("Entrada inválida.\n");
   }
-  error = ext2fs_close(*gagatinho);
+  error = ext2fs_close(gagatinho);
   if(error != 0){
     printf("Erro no fechamento do sistema de arquivos.\n");
   }
